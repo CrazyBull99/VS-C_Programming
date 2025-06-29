b069552f08a12dcf520e9e95577394624348a2bf
Max Lehmann <xmaxix99@gmx.de>
2025-06-29 21:14:29 +0200

    created new variables for variable widget design. made buttons out of widgets

--- stay_healthy/lib/main.dart
+++ stay_healthy/lib/main.dart
@@ -31,25 +31,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
-//
-  //void _incrementCounter() {
-  //  setState(() {
-  //    _counter++;
-  //  });
-  //}
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsetsDirectional.fromSTEB(0.0, 25.0, 0.0, 0.0),
+        padding: const EdgeInsetsDirectional.fromSTEB(0.0, 100.0, 0.0, 0.0),
         child: Column(
           children: [
-            TrackingElement(),
-            TrackingElement(),
-            TrackingElement(),
-          ],
+            TrackingElement(color: Color(0x40FFFFFF), iconData: Icons.directions_run, unit: "m", max:5000),
+            TrackingElement(color: Color(0x4013199A), iconData: Icons.local_drink, unit: "ml", max:3000),
+            TrackingElement(color: Color(0x40DA1717), iconData: Icons.dining, unit: "kcal", max: 2000),
+          ]
         )
 
       ),
@@ -58,45 +52,59 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class TrackingElement extends StatefulWidget {
-  const TrackingElement({super.key});
+  const TrackingElement({super.key, required this.color, required this.iconData, required this.unit, required this.max});
 
+  final Color color;
+  final IconData iconData;
+  final String unit;
+  final double max;
   @override
   State<TrackingElement> createState() => _TrackingElementState();
 }
 
 class _TrackingElementState extends State<TrackingElement> {
+  int _counter = 0;
+  double _progress = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 200;
+      _progress = _counter / widget.max;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return InkWell(
+        onTap: _incrementCounter,
+        child: Column(
+        children: <Widget>[
         Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(32.0, 25.0, 32.0, 25.0),
+            padding: EdgeInsetsDirectional.fromSTEB(32.0, 50.0, 32.0, 50.0),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Icon(
-                    Icons.directions_run,
-                    color: Colors.white70,
-                    size: 50,
-                  ),
+                children: <Widget>[
+                  Icon(widget.iconData, color: Colors.white70, size: 50,),
                   Text(
-                    '1000 / 5000 m',
-                    style: TextStyle(color: Colors.white70, fontSize: 35),
+                    '$_counter / ${widget.max.toInt()} ${widget.unit}',
+                    style: const TextStyle(color: Colors.white70, fontSize: 35),
                   )
                 ]
             )
         ),
         Padding(
             padding: EdgeInsetsGeometry.symmetric(horizontal: 5.0, vertical: 0.0),
-            child: const LinearProgressIndicator(
-              value: 0.5,
-              color: Colors.green,
-              backgroundColor: Color(0x40FFFFFF),
+            child: LinearProgressIndicator(
+              value: _progress,
+              color: widget.color,
+              backgroundColor: const Color(0x40FFFFFF),
               minHeight: 12.0,
             )
 
         )
       ],
+    )
     );
   }
 }
